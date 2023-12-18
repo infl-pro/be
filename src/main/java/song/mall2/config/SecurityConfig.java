eed3cbb7e4296c142582ac3a7a5a03d414749016
song windows <dkclasltmf@naver.com>
2023-12-18 11:27:51 +0900

    feat: security 비활성화

---
+++ src/main/java/song/mall2/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package song.mall2.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .formLogin(login -> login.disable())
+                .logout(logout -> logout.disable())
+                .build();
+    }
+}
