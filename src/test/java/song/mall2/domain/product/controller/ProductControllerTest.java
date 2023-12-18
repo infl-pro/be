1a017725fce8f2b3711cb2f9893e3bc6b5dd3b43
song windows <dkclasltmf@naver.com>
2023-12-18 19:49:33 +0900

    test: 상품 등록 요청 테스트

--- src/test/java/song/mall2/domain/product/controller/ProductControllerTest.java
+++ src/test/java/song/mall2/domain/product/controller/ProductControllerTest.java
@@ -1,16 +1,19 @@
 package song.mall2.domain.product.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import song.mall2.domain.product.dto.SaveProductDto;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -27,14 +30,22 @@ class ProductControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
     @WithUserDetails("a")
     void postSave() throws Exception {
+        SaveProductDto saveProductDto = new SaveProductDto();
+        saveProductDto.setName("test name");
+        saveProductDto.setPrice(10000);
+        saveProductDto.setDescription("test description");
+        saveProductDto.setStockQuantity(100);
+
+        String content = objectMapper.writeValueAsString(saveProductDto);
+
         mockMvc.perform(post("/product/save")
-                        .param("name", "test name")
-                        .param("price", "10000")
-                        .param("description", "test description")
-                        .param("stockQuantity", "100"))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.productId").exists());
     }
