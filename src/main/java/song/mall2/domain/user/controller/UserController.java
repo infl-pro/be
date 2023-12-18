aee31b652e0b217f04875e9717adce3c376d7b58
song windows <dkclasltmf@naver.com>
2023-12-18 22:52:04 +0900

    refactor: 회원가입 응답 변경
    - void, @ResponseStatus -> response entity

--- src/main/java/song/mall2/domain/user/controller/UserController.java
+++ src/main/java/song/mall2/domain/user/controller/UserController.java
@@ -3,6 +3,7 @@ package song.mall2.domain.user.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import song.mall2.domain.user.dto.SignupDto;
@@ -16,9 +17,10 @@ import song.mall2.domain.user.service.UserService;
 public class UserController {
     private final UserService userService;
 
-    @ResponseStatus(HttpStatus.OK)
     @PostMapping("/save")
-    public void postSave(@RequestBody SignupDto signupDto) {
+    public ResponseEntity<Object> postSave(@RequestBody SignupDto signupDto) {
         userService.save(signupDto);
+
+        return ResponseEntity.ok().build();
     }
 }
