29f0db9a2ddd3619aaef15e5142a351149aaf2f1
song windows <dkclasltmf@naver.com>
2023-12-18 19:53:28 +0900

    refactor: 회원가입 요청 매핑 애노테이션 변경
    - json요청으로 변경

--- src/main/java/song/mall2/domain/user/controller/UserController.java
+++ src/main/java/song/mall2/domain/user/controller/UserController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 import song.mall2.domain.user.dto.SignupDto;
 import song.mall2.domain.user.service.UserService;
 
@@ -21,7 +18,7 @@ public class UserController {
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/save")
-    public void postSave(SignupDto signupDto) {
+    public void postSave(@RequestBody SignupDto signupDto) {
         userService.save(signupDto);
     }
 }
--- src/test/java/song/mall2/domain/user/controller/UserControllerTest.java
+++ src/test/java/song/mall2/domain/user/controller/UserControllerTest.java
@@ -1,12 +1,15 @@
 package song.mall2.domain.user.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import song.mall2.domain.user.dto.SignupDto;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -23,12 +26,19 @@ class UserControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
     void postSave() throws Exception {
+        SignupDto signupDto = new SignupDto();
+        signupDto.setUsername("testUsername");
+        signupDto.setPassword("testPassword");
+        signupDto.setAddress("testAddress");
+        String content = objectMapper.writeValueAsString(signupDto);
+
         mockMvc.perform(post("/user/save")
-                        .param("username", "testUsername")
-                        .param("password", "testPassword")
-                        .param("address", "testAddress"))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
                 .andExpect(status().isOk());
     }
 
