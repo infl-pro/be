c76c650e5e372d15cac3b2efd8d40d32010d3411
song windows <dkclasltmf@naver.com>
2023-12-18 14:33:33 +0900

    test: 회원가입 테스트

---
+++ src/test/java/song/mall2/domain/user/service/UserServiceTest.java
@@ -0,0 +1,33 @@
+package song.mall2.domain.user.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import song.mall2.domain.user.dto.SignupDto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+@TestPropertySource(properties = {
+        "spring.profiles.active=test",
+        "JASYPT_ENCRYPTOR_PASSWORD=test"
+})
+class UserServiceTest {
+    @Autowired
+    UserService userService;
+
+    @Test
+    void save() {
+        SignupDto signupDto = new SignupDto();
+        signupDto.setUsername("testUsername");
+        signupDto.setPassword("testPassword");
+        signupDto.setAddress("testAddress");
+
+        Assertions.assertDoesNotThrow(() -> userService.save(signupDto));
+    }
+
+}
