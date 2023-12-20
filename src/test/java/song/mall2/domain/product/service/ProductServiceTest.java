e72810c15e42a82831c62ec6a40818c7fa8638a2
song windows <dkclasltmf@naver.com>
2023-12-20 23:21:30 +0900

    test: 상품 조회 테스트

--- src/test/java/song/mall2/domain/product/service/ProductServiceTest.java
+++ src/test/java/song/mall2/domain/product/service/ProductServiceTest.java
@@ -1,6 +1,7 @@
 package song.mall2.domain.product.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,8 +20,10 @@ class ProductServiceTest {
     @Autowired
     ProductService productService;
 
-    Long userId = 1L;
+    Long userAId = 1L;
+    Long productAId = 1L;
 
+    @DisplayName("상품 등록")
     @Test
     void saveProduct() {
         SaveProductDto saveProductDto = new SaveProductDto();
@@ -29,7 +32,13 @@ class ProductServiceTest {
         saveProductDto.setDescription("test description");
         saveProductDto.setStockQuantity(100);
 
-        assertDoesNotThrow(() -> productService.saveProduct(userId, saveProductDto));
+        assertDoesNotThrow(() -> productService.saveProduct(userAId, saveProductDto));
+    }
+
+    @DisplayName("상품 상세 조회")
+    @Test
+    void getProduct() {
+        assertDoesNotThrow(() -> productService.getProduct(productAId));
     }
 
 }
