b8f111689edab7eeb223bc0c4e26d878b9492e2a
song windows <dkclasltmf@naver.com>
2023-12-20 16:01:44 +0900

    test: 주문 조회 요청, 주문 상품 리스트 조회 요청, 주문 상품 조회 요청 테스트

--- src/test/java/song/mall2/domain/order/controller/OrderControllerTest.java
+++ src/test/java/song/mall2/domain/order/controller/OrderControllerTest.java
@@ -1,7 +1,9 @@
 package song.mall2.domain.order.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,13 +12,14 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.MvcResult;
 import song.mall2.domain.order.dto.SaveOrderProductDto;
 import song.mall2.domain.order.dto.SaveOrdersDto;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
@@ -33,9 +36,13 @@ class OrderControllerTest {
     ObjectMapper objectMapper = new ObjectMapper();
     Long productAId = 1L;
     Long productBId = 2L;
+    Long ordersId = 1L;
+    Long orderProductId = 1L;
 
+    @DisplayName("주문 생성")
     @Test
     @WithUserDetails("a")
+    @Transactional
     void postSaveOrder() throws Exception {
         SaveOrderProductDto saveOrderProductDto1 = createOrderProductDto(productAId, 10);
         SaveOrderProductDto saveOrderProductDto2 = createOrderProductDto(productBId, 5);
@@ -49,6 +56,44 @@ class OrderControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(content))
                 .andExpect(status().isOk());
+    }
+
+    @DisplayName("주문 조회")
+    @Test
+    @WithUserDetails("a")
+    void getOrderList() throws Exception {
+        MvcResult result = mockMvc.perform(get("/orders"))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        log.info("{}", content);
+    }
+
+    @DisplayName("주문 상품 조회")
+    @Test
+    @WithUserDetails("a")
+    void getOrderProductList() throws Exception {
+        MvcResult result = mockMvc.perform(get("/orders/{ordersId}", ordersId))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        log.info("{}", content);
+    }
+
+    @Test
+    @WithUserDetails("a")
+    void getOrderProduct() throws Exception {
+        MvcResult result = mockMvc.perform(get("/orders/orderProduct/{orderProductId}", orderProductId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(orderProductId))
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        log.info("{}", content);
     }
 
     private SaveOrderProductDto createOrderProductDto(Long productId, Integer quantity) {
