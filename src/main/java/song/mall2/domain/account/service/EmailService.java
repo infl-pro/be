e105e2414894a958398e6fb321c2f9f2eb624399
song windows <dkclasltmf@naver.com>
2024-01-06 17:32:56 +0900

    feat: 이메일 서비스

---
+++ src/main/java/song/mall2/domain/account/service/EmailService.java
@@ -0,0 +1,25 @@
+package song.mall2.domain.account.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender emailSender;
+
+    public void sendMail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("dkclasltmf@naver.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+
+        log.info("[send email] to = {}, message = {}", message.getTo(), message.getText());
+        emailSender.send(message);
+    }
+}
---
+++ src/test/java/song/mall2/domain/account/service/EmailServiceTest.java
@@ -0,0 +1,24 @@
+package song.mall2.domain.account.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@Slf4j
+@SpringBootTest
+@TestPropertySource(properties = {
+        "spring.profiles.active=test",
+        "JASYPT_ENCRYPTOR_PASSWORD=test"
+})
+class EmailServiceTest {
+    @Autowired
+    EmailService emailService;
+
+    @Test
+    void sendMail() {
+        emailService.sendMail("dkclasltmf@naver.com", "test email", "test");
+    }
+
+}
