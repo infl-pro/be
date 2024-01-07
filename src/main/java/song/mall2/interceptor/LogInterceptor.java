6937b9672a7c62eae278bc34b4915c21d9de00b3
song windows <dkclasltmf@naver.com>
2024-01-07 15:38:10 +0900

    feat: 로그 인터셉터

---
+++ src/main/java/song/mall2/interceptor/LogInterceptor.java
@@ -0,0 +1,40 @@
+package song.mall2.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class LogInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String ipAddress = request.getHeader("X-Forwarded-For");
+        if (ipAddress == null) {
+            ipAddress = request.getRemoteAddr();
+        }
+        if (ipAddress.indexOf(':') >= 0) {
+            ipAddress = ipAddress.split(":")[3];
+        }
+
+        String requestURI = request.getRequestURI();
+
+        String name = null;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            name = authentication.getName();
+        }
+
+        String method = request.getMethod();
+
+        log.info("[Log Interceptor] ipAddress = {}, uri = {}, username = {}, method = {}", ipAddress, requestURI, name, method);
+
+        return true;
+    }
+}
