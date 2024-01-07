ebca6326bfdadd764d85ff216d6b3625180332bd
song windows <dkclasltmf@naver.com>
2024-01-07 15:50:00 +0900

    feat: 인터셉터 설정

--- src/main/java/song/mall2/config/WebConfig.java
+++ src/main/java/song/mall2/config/WebConfig.java
@@ -1,14 +1,20 @@
 package song.mall2.config;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import song.mall2.interceptor.LogInterceptor;
 
 @Slf4j
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer{
+    private final LogInterceptor logInterceptor;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -16,5 +22,13 @@ public class WebConfig implements WebMvcConfigurer{
                 .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(),
                         HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name())
                 .allowedHeaders("*");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
     }
 }
