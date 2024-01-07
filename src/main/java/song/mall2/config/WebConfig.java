dbb2da80174aafe03645c6fa9a0460d337ed9c63
song windows <dkclasltmf@naver.com>
2024-01-07 15:40:41 +0900

    refactor: cors설정 변경

--- src/main/java/song/mall2/config/WebConfig.java
+++ src/main/java/song/mall2/config/WebConfig.java
@@ -1,23 +1,20 @@
 package song.mall2.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Slf4j
 @Configuration
-public class WebConfig {
-
-    @Bean
-    public WebMvcConfigurer mvcConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000");
-            }
-        };
+public class WebConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(),
+                        HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name())
+                .allowedHeaders("*");
     }
 }
