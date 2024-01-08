4b2a06b7f49441cff094a5997cf15596e07bbef6
song windows <dkclasltmf@naver.com>
2024-01-08 16:06:05 +0900

    refactor: 초기 데이터 수정

--- src/main/java/song/mall2/Init.java
+++ src/main/java/song/mall2/Init.java
@@ -1,25 +1,25 @@
 package song.mall2;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import song.mall2.domain.account.service.AccountService;
-import song.mall2.domain.cart.service.CartService;
+import song.mall2.domain.cart.entity.Cart;
+import song.mall2.domain.cart.repository.CartJpaRepository;
 import song.mall2.domain.order.entity.OrderProduct;
 import song.mall2.domain.order.entity.Orders;
 import song.mall2.domain.order.repository.OrderProductJpaRepository;
 import song.mall2.domain.order.repository.OrdersJpaRepository;
 import song.mall2.domain.payment.entity.Payment;
 import song.mall2.domain.payment.repository.PaymentJpaRepository;
-import song.mall2.domain.product.dto.SaveProductDto;
 import song.mall2.domain.product.entity.Product;
 import song.mall2.domain.product.repository.ProductJpaRepository;
-import song.mall2.domain.product.service.ProductService;
-import song.mall2.domain.account.dto.UserSignupDto;
 import song.mall2.domain.user.entity.User;
+import song.mall2.domain.user.entity.UserRole;
 import song.mall2.domain.user.repository.UserJpaRepository;
-import song.mall2.domain.user.service.UserService;
+import song.mall2.domain.user.repository.UserRoleJpaRepository;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -42,70 +42,70 @@ public class Init {
     @Component
     @RequiredArgsConstructor
     private static class InitService {
-        private final AccountService accountService;
-        private final UserService userService;
-        private final ProductService productService;
-        private final CartService cartService;
-        private final OrdersJpaRepository ordersRepository;
+        private final PasswordEncoder passwordEncoder;
         private final UserJpaRepository userRepository;
+        private final UserRoleJpaRepository userRoleRepository;
         private final ProductJpaRepository productRepository;
+        private final CartJpaRepository cartRepository;
+        private final OrdersJpaRepository ordersRepository;
         private final OrderProductJpaRepository orderProductRepository;
         private final PaymentJpaRepository paymentRepository;
 
+        @Transactional
         public void setData() {
-            Long userA = saveUser("a", "a", "nameA", "test1@email.com");
-            Long userB = saveUser("b", "b", "nameB", "test1@email.com");
+            User userA = saveUser("a", "a", "nameA", "test1@email.com");
+            User userB = saveUser("b", "b", "nameB", "test1@email.com");
 
-            accountService.grantRole(userA, ROLE_SELLER.name());
+            Product productA = saveProduct(userA, "productA", 10, "This is productA", "/file/downloadFile/spring.png", 1000);
+            Product productB = saveProduct(userA, "productB", 5, "This is productB", "/file/downloadFile/security.png", 300);
 
-            Long productA = saveProduct(userA, "productA", 10, "This is productA", "/file/downloadFile/spring.png", 1000);
-            Long productB = saveProduct(userA, "productB", 5, "This is productB", "/file/downloadFile/security.png", 300);
+            addCart(userA, productA, 10);
+            addCart(userA, productB, 10);
 
-            cartService.addCart(userA, productA, 20);
-            cartService.addCart(userA, productB, 40);
-
-            Long ordersId = saveOrder(userA, productA, 10);
+            saveOrder(userA, productA, 10);
         }
 
-        private Long saveUser(String username, String password, String name, String email) {
-            UserSignupDto userSignupDto = new UserSignupDto();
-            userSignupDto.setUsername(username);
-            userSignupDto.setPassword(password);
-            userSignupDto.setName(name);
-            userSignupDto.setEmail(email);
+        private User saveUser(String username, String password, String name, String email) {
+            User user = User.create(username, passwordEncoder.encode(password), name, email);
 
-            return accountService.saveUser(userSignupDto);
+            User saveUser = userRepository.save(user);
+
+            grantRole(user);
+
+            return saveUser;
         }
 
-        private Long saveProduct(Long userId, String name, Integer price, String description, String thumbnailUrl, Integer stockQuantity) {
-            SaveProductDto saveProductDto = new SaveProductDto();
-            saveProductDto.setName(name);
-            saveProductDto.setPrice(price);
-            saveProductDto.setDescription(description);
-            saveProductDto.setThumbnailUrl(thumbnailUrl);
-            saveProductDto.setStockQuantity(stockQuantity);
-            saveProductDto.setCategoryName(Product.Category.A.name());
-
-            return productService.saveProduct(userId, saveProductDto).getProductId();
+        private void grantRole(User user) {
+            UserRole userRole = UserRole.create(user, ROLE_USER.name());
+            userRoleRepository.save(userRole);
         }
 
+        private Product saveProduct(User userId, String name, Integer price, String description, String thumbnailUrl, Integer stockQuantity) {
+            Product product = Product.create(userId, name, price, description, thumbnailUrl, null, stockQuantity, Product.Category.A.name());
 
-        private Long saveOrder(Long userId, Long productId, Integer quantity) {
-            User userA = userRepository.findById(userId).get();
-            Orders orders = Orders.create(userA);
+            return productRepository.save(product);
+        }
+
+        private Cart addCart(User user, Product product, Integer quantity) {
+            Cart cart = Cart.create(user, product, quantity);
+
+            return cartRepository.save(cart);
+        }
+
+        private Orders saveOrder(User user, Product product, Integer quantity) {
+            Orders orders = Orders.create(user);
 
             List<OrderProduct> orderProductList = new ArrayList<>();
-            Product product = productRepository.findById(productId).get();
-            OrderProduct orderProduct = OrderProduct.create(orders, product, userA, quantity);
+            OrderProduct orderProduct = OrderProduct.create(orders, product, user, quantity);
             orderProductList.add(orderProduct);
 
             Orders saveOrders = ordersRepository.save(orders);
             List<OrderProduct> saveOrderProductList = orderProductRepository.saveAll(orderProductList);
 
-            Payment payment = Payment.of(userA, orders, "testPayment", "PAID", orderProduct.getAmount(), DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm").format(LocalDateTime.now()), null, null);
+            Payment payment = Payment.of(user, orders, "testPayment", "PAID", orderProduct.getAmount(), DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm").format(LocalDateTime.now()), null, null);
             Payment savePayment = paymentRepository.save(payment);
 
-            return saveOrders.getId();
+            return saveOrders;
         }
     }
 }
