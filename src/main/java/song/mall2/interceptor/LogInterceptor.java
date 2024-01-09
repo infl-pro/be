e587a6e9363070a4a83b7d1376348227e0441671
song windows <dkclasltmf@naver.com>
2024-01-09 23:22:00 +0900

    chore: 인터셉터 컴포넌트 제거

--- src/main/java/song/mall2/interceptor/LogInterceptor.java
+++ src/main/java/song/mall2/interceptor/LogInterceptor.java
@@ -6,11 +6,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Slf4j
-@Component
 @RequiredArgsConstructor
 public class LogInterceptor implements HandlerInterceptor {
     @Override
