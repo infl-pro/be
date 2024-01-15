8a5cc7d3d5113e219d2189ed710868d052db9426
song windows <dkclasltmf@naver.com>
2024-01-15 12:13:26 +0900

    feat: 파일 업로드 테스트

--- src/main/java/song/mall2/domain/file/controller/FileController.java
+++ src/main/java/song/mall2/domain/file/controller/FileController.java
@@ -7,15 +7,14 @@ import org.springframework.core.io.UrlResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import song.mall2.domain.file.dto.UploadFileDto;
 import song.mall2.domain.file.service.FileService;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -25,10 +24,14 @@ public class FileController {
     private final FileService fileService;
 
     @PostMapping("/uploadFile")
-    public ResponseEntity<UploadFileDto> postUploadFile(MultipartFile upload) throws IOException {
-        UploadFileDto uploadFile = fileService.upload(upload);
+    public ResponseEntity<List<UploadFileDto>> postUploadFile(@RequestParam("files") List<MultipartFile> files) throws IOException {
+        List<UploadFileDto> uploadFileDtoList = new ArrayList<>();
+        for (MultipartFile multipartFile : files) {
+            UploadFileDto uploadFile = fileService.upload(multipartFile);
+            uploadFileDtoList.add(uploadFile);
+        }
 
-        return ResponseEntity.ok(uploadFile);
+        return ResponseEntity.ok(uploadFileDtoList);
     }
 
     @GetMapping("/downloadFile/{savedFileName}")
@@ -48,5 +51,17 @@ public class FileController {
             return "image/png";
         }
         return "application/octet-stream";
+    }
+
+    @GetMapping("/getfile")
+    public String getFile() {
+
+        return "filepage";
+    }
+
+    @GetMapping("/getfiles")
+    public String getFiles() {
+
+        return "filespage";
     }
 }
