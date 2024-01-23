df52f0bff71e25e8a6985db25bc4791031a06a83
song windows <dkclasltmf@naver.com>
2024-01-23 17:10:50 +0900

    chore: user 컨트롤러 주석

--- src/main/java/song/mall2/domain/user/controller/UserController.java
+++ src/main/java/song/mall2/domain/user/controller/UserController.java
@@ -2,15 +2,9 @@ package song.mall2.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import song.mall2.domain.user.dto.RequestUpdatePassword;
-import song.mall2.domain.user.dto.RequestUpdateProfile;
-import song.mall2.domain.user.dto.UserDto;
 import song.mall2.domain.user.service.UserService;
-import song.mall2.security.authentication.userprincipal.UserPrincipal;
 
 @Slf4j
 @Controller
@@ -20,26 +14,26 @@ import song.mall2.security.authentication.userprincipal.UserPrincipal;
 public class UserController {
     private final UserService userService;
 
-    @GetMapping
-    public ResponseEntity<UserDto> getUser(@AuthenticationPrincipal UserPrincipal userPrincipal) {
-        UserDto user = userService.getUser(userPrincipal.getId());
-
-        return ResponseEntity.ok(user);
-    }
-
-    @PatchMapping("/updateProfile")
-    public ResponseEntity<UserDto> postUpdateProfile(@AuthenticationPrincipal UserPrincipal userPrincipal,
-                                                     @RequestBody RequestUpdateProfile requestUpdateProfile) {
-        UserDto user = userService.updateProfile(userPrincipal.getId(), requestUpdateProfile.getProfileUrl());
-
-        return ResponseEntity.ok(user);
-    }
-
-    @PatchMapping("/updatePassword")
-    public ResponseEntity<UserDto> postUpdatePassword(@AuthenticationPrincipal UserPrincipal userPrincipal,
-                                                      @RequestBody RequestUpdatePassword requestUpdatePassword) {
-        UserDto user = userService.updatePassword(userPrincipal.getId(), requestUpdatePassword.getOriginalPassword(), requestUpdatePassword.getNewPassword(), requestUpdatePassword.getConfirmNewPassword());
-
-        return ResponseEntity.ok(user);
-    }
+//    @GetMapping
+//    public ResponseEntity<UserDto> getUser(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+//        UserDto user = userService.getUser(userPrincipal.getId());
+//
+//        return ResponseEntity.ok(user);
+//    }
+//
+//    @PatchMapping("/updateProfile")
+//    public ResponseEntity<UserDto> postUpdateProfile(@AuthenticationPrincipal UserPrincipal userPrincipal,
+//                                                     @RequestBody RequestUpdateProfile requestUpdateProfile) {
+//        UserDto user = userService.updateProfile(userPrincipal.getId(), requestUpdateProfile.getProfileUrl());
+//
+//        return ResponseEntity.ok(user);
+//    }
+//
+//    @PatchMapping("/updatePassword")
+//    public ResponseEntity<UserDto> postUpdatePassword(@AuthenticationPrincipal UserPrincipal userPrincipal,
+//                                                      @RequestBody RequestUpdatePassword requestUpdatePassword) {
+//        UserDto user = userService.updatePassword(userPrincipal.getId(), requestUpdatePassword.getOriginalPassword(), requestUpdatePassword.getNewPassword(), requestUpdatePassword.getConfirmNewPassword());
+//
+//        return ResponseEntity.ok(user);
+//    }
 }
