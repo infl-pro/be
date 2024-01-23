ecc4fb598e7a05e173df5caa8e93e67b06e8b11c
song windows <dkclasltmf@naver.com>
2024-01-23 17:35:51 +0900

    test: 좋아요 테스트

--- src/test/java/song/mall2/domain/favorite/service/FavoriteServiceTest.java
+++ src/test/java/song/mall2/domain/favorite/service/FavoriteServiceTest.java
@@ -1,12 +1,15 @@
-package song.mall2.domain.favorite.repository;
+package song.mall2.domain.favorite.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import song.mall2.domain.favorite.dto.FavoriteDto;
+import song.mall2.domain.favorite.repository.FavoriteJpaRepository;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
@@ -14,17 +17,30 @@ import static org.assertj.core.api.Assertions.*;
         "spring.profiles.active=test",
         "JASYPT_ENCRYPTOR_PASSWORD=test"
 })
-class FavoriteJpaRepositoryTest {
+class FavoriteServiceTest {
+    @Autowired
+    FavoriteService favoriteService;
     @Autowired
     FavoriteJpaRepository favoriteRepository;
 
     Long userAId = 1L;
     Long product1Id = 1L;
+    Long product5Id = 5L;
 
     @Test
-    void exists() {
+    void addFavorite() {
+        FavoriteDto favorite = favoriteService.addFavorite(userAId, product5Id);
+
+        assertThat(favorite.getUserId())
+                .isEqualTo(userAId);
+    }
+
+    @Test
+    void deleteFavorite() {
+        favoriteService.deleteFavorite(userAId, product1Id);
+
         assertThat(favoriteRepository.findByUserIdAndProductId(userAId, product1Id))
-                .isNotEmpty();
+                .isEmpty();
     }
 
 }
