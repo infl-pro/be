90ea75c345ce803f5504040c822ded7e153a0f82
song windows <dkclasltmf@naver.com>
2024-02-14 19:35:45 +0900

    chore: 주석 삭제

--- src/main/java/song/mall2/Mall2Application.java
+++ src/main/java/song/mall2/Mall2Application.java
@@ -20,7 +20,6 @@ public class Mall2Application {
 	}
 	@Bean
 	public ServletWebServerFactory servletContainer() {
-		// Enable SSL Trafic
 		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
 			@Override
 			protected void postProcessContext(Context context) {
@@ -33,17 +32,11 @@ public class Mall2Application {
 			}
 		};
 
-		// Add HTTP to HTTPS redirect
 		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
 
 		return tomcat;
 	}
 
-	/*
-    We need to redirect from HTTP to HTTPS. Without SSL, this application used
-    port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
-    redirected to HTTPS on 8443.
-     */
 	private Connector httpToHttpsRedirectConnector() {
 		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
 		connector.setScheme("http");
