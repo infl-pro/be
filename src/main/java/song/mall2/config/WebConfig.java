4d1b3900e7d83ff25614f30b9e84c265c48bf9ca
song windows <dkclasltmf@naver.com>
2024-02-14 14:05:27 +0900

    fix: refreshToken header 추가

--- src/main/java/song/mall2/config/WebConfig.java
+++ src/main/java/song/mall2/config/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(),
                         HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name(),
                         HttpMethod.PATCH.name(), HttpMethod.PUT.name())
-                .allowedHeaders("Content-Type", "Authorization", "X-Requested-With")
+                .allowedHeaders("Content-Type", "Authorization", "X-Requested-With", "refreshToken")
                 .allowCredentials(true);
     }
 
--- src/main/java/song/mall2/security/cors/CorsFactory.java
+++ src/main/java/song/mall2/security/cors/CorsFactory.java
@@ -10,7 +10,7 @@ public class CorsFactory {
     public static HttpServletResponse setCors(HttpServletRequest request, HttpServletResponse response) {
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Method", "GET, POST, DELETE, OPTIONS, PUT, PATCH");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, refreshToken");
         response.setHeader("Access-Control-Allow-Credentials", "true");
 
         return response;
